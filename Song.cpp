61f2b0266eb124986930e4fa44b733308835eed9
justin-g-wang <127655051+justin-g-wang@users.noreply.github.com>
2024-03-13 13:49:45 -0700

    Delete Song.cpp

--- Song.cpp
+++
@@ -1,4 +0,0 @@
-    #include "Song.h"
-
-    Song::Song(const std::string& artist, const std::string& albumName, const std::string& songName, const std::string& popularity, const std::string& genre)
-        : artist(artist), albumName(albumName), songName(songName), popularity(popularity), genre(genre) {}
