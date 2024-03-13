9d7df6dd3f060d38c11862f99687abc3061acf69
justin-g-wang <127655051+justin-g-wang@users.noreply.github.com>
2024-03-13 13:50:44 -0700

    Delete UserAuthentication.cpp

--- UserAuthentication.cpp
+++
@@ -1,8 +0,0 @@
-#include "UserAuthentication.h"
-
-UserAuthentication::UserAuthentication(UserStorage& storage) : storage(storage) {}
-
-bool UserAuthentication::authenticate(const std::string& username, const std::string& password) 
-{
-    return storage.validateUser(username, password);
-}
