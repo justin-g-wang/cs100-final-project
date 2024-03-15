3afd4c3756428d7b3c1c5b1267239dc36d92ad6a
aakifiq <aakif.iqbal@icloud.com>
2024-03-15 09:36:36 -0700

    committing the main recc file

--- src/Recc.cpp
+++ src/Recc.cpp
@@ -1,5 +1,6 @@
 #include "../header/Recc.h"
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <cstring>
 #include <algorithm>
@@ -7,7 +8,40 @@
 
 using namespace std;
 
-void Recc::viewRecc(SongCollection& collection, std::vector<Song>& recc) {
+void Recc::saveToFile() const {
+        std::ofstream file("recc.txt");
+    for (const auto& song : recc) {
+        file << song.artist << ","
+             << song.albumName << ","
+             << song.songName << ","
+             << song.popularity << ","
+             << song.genre << ","
+             << song.rating << std::endl;
+    }
+    file.close();
+    }
+
+void Recc::loadFromFile() {
+    std::ifstream file("recc.txt");
+    std::string line;
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string artist, albumName, songName, popularity, genre, rating;
+            
+        std::getline(ss, artist, ',');
+        std::getline(ss, albumName, ',');
+        std::getline(ss, songName, ',');
+        std::getline(ss, popularity, ',');
+        std::getline(ss, genre, ',');
+        std::getline(ss, rating);
+                        Song song(artist, albumName, songName, popularity, genre, rating);
+        recc.push_back(song);
+    }
+    file.close();
+    }
+
+
+void Recc::viewRecc(SongCollection& collection) {
         std::cout << "===== Reccomendations =====" << std::endl;
         for (auto &song : recc) {
             std::cout << "Title: " << song.songName << ", Artist: " << song.artist << ", Album: " << song.albumName << std::endl;
@@ -19,7 +53,11 @@ std::vector<Song>& Recc::getRecc(){
         return recc;
     }
 
-void Recc::ReccBySong(SongCollection& collection, Diary& userDiary, std::vector<Song>& recc) {
+void Recc::addSong(const Song userSong) {
+    recc.push_back(userSong);
+}
+
+void Recc::ReccBySong(SongCollection& collection, Diary& userDiary) {
     // first, its gonna go through the diary vector and stop everytime it finds a song rated at 8 or higher
     // then, everytime it stops, its going to look at the genre of the song
     // then, its gonna iterate through the RECC Vector (formerly a copy of the whole collection) and find a song with that same genre
@@ -28,10 +66,9 @@ void Recc::ReccBySong(SongCollection& collection, Diary& userDiary, std::vector<
     // then it will add the recommendations to a new vector of recommended music
     // "Enter (L) to load more recommendations, otherwise press (Q) to quit."
 
-    string ratingLevel;
-
     cout << "Recommending by Song Name." << endl;
-    std::cout << "Enter a Rating level (1.0 - 10.0): ";
+    std::cout << "Enter a Rating level (1 - 10): ";
+    string ratingLevel;
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::getline(std::cin, ratingLevel); 
 
@@ -49,15 +86,25 @@ void Recc::ReccBySong(SongCollection& collection, Diary& userDiary, std::vector<
 
             for (const auto& song : allSongs) 
             {
-                if (tempGenre == song.genre) {
+                if (song.genre == tempGenre + "\r") {
                     if (reccCounter < 5) {
+                        addSong(song);
                         ++reccCounter;
                         std::cout << "Artist: " << song.artist
                         << ", Album: " << song.albumName
                         << ", Song: " << song.songName << std::endl;
                     }
                     else if (reccCounter == 5) {
-                        cout << "Enter (L) to load more recommendations, otherwise press (Q) to quit." << endl;
+                        cout << "\n" << "Enter (L) to load more recommendations, otherwise press (Q) to quit." << endl;
+                        string userKey;
+                        std::getline(std::cin, userKey);
+
+                        if (userKey == "L") {
+                            reccCounter = 0;
+                        }
+                        else if (userKey == "Q") {
+                            break;
+                        } 
                     }
                 }
             }
@@ -88,7 +135,7 @@ void Recc::promptForRecc(SongCollection& collection, Diary& userDiary, std::vect
                 //ReccByAlbum(collection);
                 isValidChoice = true;
             } else if (userChoice == "3" || userChoice == "song name") {
-                ReccBySong(collection, userDiary, recc);
+                ReccBySong(collection, userDiary);
                 isValidChoice = true;
             } else if (userChoice == "4" || userChoice == "popularity") {
                 //ReccByPopularity(collection);
