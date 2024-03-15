88a0a7615df82388e419b53ba7b811d804767ff9
justin-g-wang <fromjustinwang@gmail.com>
2024-03-15 15:31:54 -0700

    removed executable

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,9 +1,9 @@
 # CMake generated Testfile for 
 # Source directory: /class/classes/jwang798/final-project-jwang798-hfawa001-feshe001-aiqba016
-# Build directory: /class/classes/jwang798/final-project-jwang798-hfawa001-feshe001-aiqba016
+# Build directory: /data/class/classes/jwang798/final-project-jwang798-hfawa001-feshe001-aiqba016
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(GoogleTest "/class/classes/jwang798/final-project-jwang798-hfawa001-feshe001-aiqba016/googleTest")
+add_test(GoogleTest "/data/class/classes/jwang798/final-project-jwang798-hfawa001-feshe001-aiqba016/googleTest")
 set_tests_properties(GoogleTest PROPERTIES  _BACKTRACE_TRIPLES "/class/classes/jwang798/final-project-jwang798-hfawa001-feshe001-aiqba016/CMakeLists.txt;17;add_test;/class/classes/jwang798/final-project-jwang798-hfawa001-feshe001-aiqba016/CMakeLists.txt;0;")
 subdirs("googletest")
